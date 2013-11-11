9910a82fa76e676dadf3df5fd1cdafc4bbad0949
Patrick Lenz <patrick@limited-overload.de>
2013-11-11 15:17:48 +0100

    Include opportunity cost

--- lib/autotask_api/opportunity.rb
+++ lib/autotask_api/opportunity.rb
@@ -1,6 +1,6 @@
 module AutotaskAPI
   class Opportunity < Entity
-    self.fields = [ :id, :title, :amount, :owner_resource_id ]
+    self.fields = [ :id, :title, :amount, :cost, :owner_resource_id ]
     belongs_to :owner_resource, class_name: :resource
   end
 end
