28b540339cf9cdef1b22702ada12394ef00585a9
Patrick Lenz <patrick@limited-overload.de>
2015-02-27 08:49:46 +0100

    Fix wrong association type

--- lib/autotask_api/service_call_ticket_resource.rb
+++ lib/autotask_api/service_call_ticket_resource.rb
@@ -2,6 +2,6 @@ module AutotaskAPI
   class ServiceCallTicketResource < Entity
     self.fields = [ :id, :ticket_id, :resource_id ]
     belongs_to :service_call_ticket
-    has_one :resource
+    belongs_to :resource
   end
 end
