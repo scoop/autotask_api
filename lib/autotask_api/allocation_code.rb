95dc3996306498d6193cbe3a1892517e3b6eaff9
Abhishek kant <abhi1206118@gmail.com>
2018-08-30 16:45:34 +0530

    billing_item, invoice and allocation_code entities

---
+++ lib/autotask_api/allocation_code.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module AutotaskAPI
+  # BillingItem
+  class AllocationCode < Entity
+    self.fields = %i[id external_number]
+  end
+end
---
+++ lib/autotask_api/invoice.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module AutotaskAPI
+  # Invoice
+  class Invoice < Entity
+    self.fields = %i[id user_defined_fields account_id
+                     creator_resource_id invoice_date_time
+                     create_date_time invoice_number
+                     invoice_total total_tax_value from_date
+                     order_number is_voided tax_region_name batch_id
+                     invoice_editor_template_id]
+  end
+end
